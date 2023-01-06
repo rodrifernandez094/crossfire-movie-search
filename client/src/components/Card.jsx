c5a2a52d1489a0768413e11208e8852be7db7591
rodrifernandez094 <rodrifernandez094@gmail.com>
2023-01-06 17:53:56 -0300

    Move assets to images folder

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import defaultMoviePoster from "../assets/movie-poster.png";
+import defaultMoviePoster from "../assets/images/movie-poster.png";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
